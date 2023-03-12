3382d89907f42d33660bfef17efb5fbc4b61d0d0
nadhirns <11201071@student.itk.ac.id>
2023-03-12 13:06:03 +0800

    ubah tampilan index, style.cc ga keubah kok

--- resources/views/halaman/index.blade.php
+++ resources/views/halaman/index.blade.php
@@ -1,24 +1,31 @@
 @extends('layout/app')
 
 @section('content')
-    <div class="h-100 d-flex align-items-center justify-content-center">
-        <div class="formlogin">
+        <div class="formlogin" style="
+            box-sizing: border-box;
+            width: 600px;
+            padding: 20px;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            border: solid gainsboro;
+            border-radius: 10px;
+        ">
             <form>
-                <div class="row mb-3">
-                    <label for="inputEmail3" class="col-sm-4 col-form-label">Email</label>
-                    <div class="col-sm-10">
-                        <input type="email" class="form-control" id="inputEmail3">
-                    </div>
+                <div class="mb-3 ">
+                    <label for="inputEmail3" class="form-label"></label>
+                    <input type="email" class="form-control" id="inputEmail3" placeholder="email">
                 </div>
-                <div class="row mb-3">
-                    <label for="inputPassword3" class="col-sm-4 col-form-label">Password</label>
-                        <div class="col-sm-10">
-                        <input type="password" class="form-control" id="inputPassword3">
-                    </div>
+                <div class="mb-3">
+                    <label for="inputPassword3" class="form-label"></label>
+                    <input type="password" class="form-control" id="inputPassword3" placeholder="password">
                 </div>
-                <a href="pengguna_book_hotel.html">anu</a><br>
-                <button type="submit" class="btn btn-primary">Sign in</button>
+                <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                    <button type="submit" class="btn btn-primary">
+                        <a href="/dashboard_admin">Masuk</a>
+                    </button>
+                </div>
             </form>
         </div>
-    </div>
 @endsection
